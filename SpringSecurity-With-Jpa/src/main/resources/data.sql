9e477ab9ec0f47301099b8cf717ba0cba91312a0
Debasis Maharana <59536116+MrDebasis@users.noreply.github.com>
2020-07-03 17:19:21 +0530

    Update data.sql

--- SpringSecurity-With-Jpa/src/main/resources/data.sql
+++ SpringSecurity-With-Jpa/src/main/resources/data.sql
@@ -1,7 +1,7 @@
 INSERT INTO Users(user_name,password,enabled)
-VALUES ('subhashree','$2y$12$9UCYvKKYweQxe5/xlGeFtuu1gpE6smKnL/kmnGAqpN.Iw/zYptT2y',true);
+VALUES ('Debasis','$2y$12$9UCYvKKYweQxe5/xlGeFtuu1gpE6smKnL/kmnGAqpN.Iw/zYptT2y',true);
 INSERT INTO Users(user_name,password,enabled)
-VALUES ('jyoti','$2y$12$WGD5kYVQEvkoam3t2u02fu/8BJYXMR1ppbPLr4i5mNJYsNSS9Dv0K',true);
+VALUES ('Debasis','$2y$12$WGD5kYVQEvkoam3t2u02fu/8BJYXMR1ppbPLr4i5mNJYsNSS9Dv0K',true);
 INSERT INTO Roles (Name)
 VALUES ('ADMIN');
 INSERT INTO Roles (Name)
